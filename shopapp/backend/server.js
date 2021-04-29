3a5799cffad412422b2909b49382dff877218e3a
Revan Sarbast Salih <revansarbast8@gmail.com>
2021-04-29 06:09:46 +0300

    add .env

--- shopapp/backend/server.js
+++ shopapp/backend/server.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const dotenv = require('dotenv')
-const products = require('./data/products')
-const PORT = process.env.PORT || 5000
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+const PORT = process.env.PORT || 5000;
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is runing...')
-})
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.get("/", (req, res) => {
+  res.send("API is runing...");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(PORT, console.log(`Server is runing in ${process.env.NODE_ENV} on port ${PORT}`))
-
+app.listen(
+  PORT,
+  console.log(`Server is runing in ${process.env.NODE_ENV} on port ${PORT}`)
+);
